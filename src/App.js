aa77de28f47dbe80a43cf0ae6e7c74a143f44d6c
Sandeep Reddy <Sandeep.Reddy@sapiens.com>
2021-03-06 11:25:31 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", age: "", salary: "" };
+
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeAge = this.handleChangeAge.bind(this);
+    this.handleChangeSalary = this.handleChangeSalary.bind(this);
+    this.getEmployees = this.getEmployees.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeName(event) {
+    this.setState({ name: event.target.value });
+  }
+  handleChangeAge(event) {
+    this.setState({ age: event.target.value });
+  }
+  handleChangeSalary(event) {
+    this.setState({ salary: event.target.value });
+  }
+
+  handleSubmit(event) {
+    let data = {
+      name: this.state.name,
+      salary: this.state.salary,
+      age: this.state.age,
+    };
+    fetch("https://dummy.restapiexample.com/api/v1/create", {
+      method: "POST", // or 'PUT'
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    event.preventDefault();
+  }
+
+  getEmployees(event) {
+    fetch("https://dummy.restapiexample.com/api/v1/employees", {
+      method: "get",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChangeName}
+            />
+            <div>
+              Salary:{" "}
+              <input
+                type="text"
+                value={this.state.salary}
+                onChange={this.handleChangeSalary}
+              ></input>{" "}
+            </div>
+            <div>
+              age:{" "}
+              <input
+                type="text"
+                value={this.state.age}
+                onChange={this.handleChangeAge}
+              ></input>{" "}
+            </div>
+          </label>
+          <br></br>
+          <input type="submit" value="Submit" />
+        </form>
+        <input
+          type="button"
+          value="Get Employees"
+          onClick={this.getEmployees}
+        />
+      </>
+    );
+  }
 }
-
-export default App;
